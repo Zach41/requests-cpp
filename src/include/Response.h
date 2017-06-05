30bddc2f1c4d2fe039064ebebb56031e58ddc786
Zach <zach_41@163.com>
2017-06-05 22:54:03 +0800

    error

---
+++ src/include/Response.h
@@ -0,0 +1,19 @@
+#ifndef REQUESTS_RESPONSE_H
+#define REQUESTS_RESPONSE_H
+
+#include "types.h"
+
+#include <cstdint>
+#include <string>
+
+namespace requests {
+    // class Response {
+    // private:
+    //     std::int32_t status_code;
+    //     double elapsed;
+    //     Header header;
+    //     Url url;
+
+    // }
+}
+#endif // REQUESTS_RESPONSE_H end here
--- src/include/types.h
+++ src/include/types.h
@@ -2,10 +2,23 @@
 #define REQUESTS_TYPES_H
 
 #include <string>
+#include <cctype>
+#include <map>
 #include <curl/curl.h>
 
 namespace requests {
     using Url = std::string;
+
+    struct CaseInsensitiveCompare {
+        bool operator()(const std::string &str1, const std::string &str2) const noexcept {
+            return std::lexicographical_compare(
+                str1.begin(), str1.end(), str2.begin(), str2.end(),
+                [](unsigned char ch1, unsigned char ch2) { return std::tolower(ch1) < std::tolower(ch2); }
+            );
+        }
+    };
+
+    using Header = std::map<std::string, std::string, CaseInsensitiveCompare>;
     using Response = std::string;
 
     struct CurlHolder {
